28d423bcddc0bcbc67749d1186e51c3eb2a37f30
Bruno Griveau <brunog90@gmail.com>
2026-04-05 23:55:21 +0200

    tweak

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-const GRAVITY = 1200
-const JUMP_SPEED = 600
-const ACCELERATION = 20000.0
-const MAX_VELOCITY = 300000
-const FRICTION = 4000.0
+const GRAVITY = 1240.0
+const JUMP_SPEED = 640.0
+const ACCELERATION = 2000.0
+const MAX_SPEED = 400.0      
+const FRICTION = 4000.0   
 
 func _physics_process(delta: float) -> void:
 	
@@ -13,14 +13,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= JUMP_SPEED
 		
-	if Input.is_action_just_released("jump"):
+	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = velocity.y / 4
 	
 	if direction != 0:
-		velocity.x = move_toward(0, MAX_VELOCITY * direction, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, MAX_SPEED * direction, ACCELERATION * delta)
 	else: 
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	
-	velocity.y += GRAVITY * delta
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
 	
 	move_and_slide()
