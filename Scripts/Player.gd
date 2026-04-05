bec4724ca9104d947402ceef559ea09255233830
Bruno Griveau <brunog90@gmail.com>
2026-04-05 23:28:32 +0200

    basic input

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,26 @@
-extends Node
+extends CharacterBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+const GRAVITY = 1200
+const JUMP_SPEED = 600
+const ACCELERATION = 20000.0
+const MAX_VELOCITY = 300000
+const FRICTION = 4000.0
 
 func _physics_process(delta: float) -> void:
-	pass
+	
+	var direction = Input.get_axis("left", "right")
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= JUMP_SPEED
+		
+	if Input.is_action_just_released("jump"):
+		velocity.y = velocity.y / 4
+	
+	if direction != 0:
+		velocity.x = move_toward(0, MAX_VELOCITY * direction, ACCELERATION * delta)
+	else: 
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	
+	velocity.y += GRAVITY * delta
+	
+	move_and_slide()
