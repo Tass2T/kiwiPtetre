344aa28b5a8f3c022d8c0ec52b8aca082000be09
Bruno Griveau <brunog90@gmail.com>
2026-06-18 17:25:17 +0200

    ajustement joueur

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -4,14 +4,14 @@ class_name Player extends CharacterBody2D
 @onready var ray_left: RayCast2D = $Ray_Left
 @onready var states: State_Manager = $States
 
-const GRAVITY : float = 1240.0
-const CLIMB_GRAVITY: float = 100.0
-const JUMP_SPEED: float = 1200.0
-const ACCELERATION: float = 5000.0
-const MAX_SPEED: float = 800.0      
+const GRAVITY : float = 6000.0
+const CLIMB_GRAVITY: float = 1200.0
+const JUMP_SPEED: float = 3000.0
+const ACCELERATION: float = 7800.0
+const MAX_SPEED: float = 1400.0      
 const FRICTION: float = 8000.0
-const BOUNCE_FORCE_Y: float  = 600.0
-const BOUNCE_FORCE_X: float = 400.0
+const BOUNCE_FORCE_Y: float  = 1400.0
+const BOUNCE_FORCE_X: float = 800.0
 
 var direction: float = 0
 var wall_direction: float = 0.0
