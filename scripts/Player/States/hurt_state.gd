b4d57342ca56a0c2fa0db65072b6c59a4c577ecf
Bruno Griveau <54628533+Tass2T@users.noreply.github.com>
2026-09-10 22:46:01 +0200

    hurt timeout

--- scripts/Player/States/hurt_state.gd
+++ scripts/Player/States/hurt_state.gd
@@ -2,8 +2,14 @@ class_name Hurt_State extends State
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $"../../AnimatedSprite2D"
 @onready var idle_state: Idle_State = $"../Idle"
+@onready var fall_state: Fall_State = $"../Fall"
+
+@onready var recovery_timer: Timer = $RecoveryTimer
+
+const RECOVERY_TIME: float = 0.6
 
 func Enter() -> void:
+	recovery_timer.start(RECOVERY_TIME)
 	player.velocity = Vector2.ZERO
 	animated_sprite_2d.play("hurt")
 	player.velocity.x -= 800 * player.sprite_direction
@@ -11,8 +17,13 @@ func Enter() -> void:
 
 func Physics_Process(delta: float) -> State:
 	if player.is_on_floor():
+		recovery_timer.stop()
 		return idle_state
 		
 	player.velocity.y = move_toward(player.velocity.y, player.MAX_FALLING_SPEED, player.GRAVITY * delta)
 		
 	return 
+
+
+func _on_recovery_timer_timeout() -> void:
+	player.trigger_state("fall")
